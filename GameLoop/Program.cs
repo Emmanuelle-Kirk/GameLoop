fdefa6eaaaa8d6fef46dfc7e86f9902635079980
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-19 10:36:05 -0300

    MovesNow

--- GameLoop/Program.cs
+++ GameLoop/Program.cs
@@ -13,40 +13,54 @@ namespace GameLoop
         static int y;
         //if input is??????
 
+        static void PlayerDraw()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(x, y);
+            Console.Write("p");
+        }
+        static void PlayerUpdate()
+        {
+            ConsoleKeyInfo readKeyInput = Console.ReadKey(true); //ReadKey stored
+            char input = readKeyInput.KeyChar; //extracting char
+            if (input == 'w')
+            {
+                y = y + 1;
+                //Console.WriteLine("y = " + y);
+            }
+            if (input == 'a')
+            {
+                x = x - 1;
+                //Console.WriteLine("x = " + x);
+            }
+            if (input == 's')
+            {
+                y = y - 1;
+                //Console.WriteLine("y = " + y);
+            }
+            if (input == 'd')
+            {
+                x = x + 1;
+                //Console.WriteLine("X = " + x);
+            }
+            if (x <= -1)
+            {
+                x = x + 1;
+            }
+            if (y <= -1)
+            {
+                y = y + 1;
+            }
+        }
         static void Main(string[] args)
         {
             while (gameOver == false)
             {
-                ConsoleKeyInfo readKeyInput = Console.ReadKey(true); //ReadKey stored
-                char input = readKeyInput.KeyChar; //extracting char
-
-                if (input == 'w')
-                {
-                    y = y + 1;
-                    Console.WriteLine(y);
-                }
-                if (input == 'a')
-                {
-                    x = x - 1;
-                    Console.WriteLine(x);
-                }
-                if (input == 's')
-                {
-                    y = y - 1;
-                    Console.WriteLine(y);
-                }
-                if (input == 'd')
-                {
-                    x = x + 1;
-                    Console.WriteLine(x);
-                }
-
+                PlayerUpdate();
+                PlayerDraw();
             }
-            if (gameOver == true)
-            {
-                Console.WriteLine("GAME OVER");
-                Console.ReadKey(true);
-            }
+            Console.WriteLine("GAME OVER");
+            Console.ReadKey(true);
         }
     }
 }
