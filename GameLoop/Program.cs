400c8ea2848b57abf6b681e55129ec66e345eca3
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-19 09:41:58 -0300

    UPDOWNLEFTRIGHT

--- GameLoop/Program.cs
+++ GameLoop/Program.cs
@@ -8,12 +8,45 @@ namespace GameLoop
 {
     class Program
     {
-        static bool gameOver;
+        static bool gameOver = (false);
         static int x;
         static int y;
+        //if input is??????
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Game Loop");
+            while (gameOver == false)
+            {
+                ConsoleKeyInfo readKeyInput = Console.ReadKey(true); //ReadKey stored
+                char input = readKeyInput.KeyChar; //extracting char
+
+                if (input == 'w')
+                {
+                    Console.WriteLine("UP");
+                }
+                if (input == 'a')
+                {
+                    Console.WriteLine("LEFT");
+                }
+                if (input == 's')
+                {
+                    Console.WriteLine("DOWN");
+                }
+                if (input == 'd')
+                {
+                    Console.WriteLine("RIGHT");
+                }
+
+            }
+            if (gameOver == true)
+            {
+                Console.WriteLine("GAME OVER");
+                Console.ReadKey(true);
+            }
         }
     }
 }
+    
+
+    
+
